f4ed27514260a4aee5aa8a2735050393b530662d
Maximilian Heim <MaximilianHeim@protonmail.com>
2021-10-30 18:53:15 +0200

    added parser file

---
+++ src/parser.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "token_tree.h"
+
+token_tree *parse_program(char *code) {
+	unsigned long position = 0;
+	token_tree *tree = new_token_tree();
+	while (code[position] != '\0') {
+		
+	}
+}
